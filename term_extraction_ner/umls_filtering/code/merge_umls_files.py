67372aac1f3e9afa46f3a6995741913317924314
An <an@clab>
2020-09-08 11:40:21 +0300

    Renamed get_mesh_vocab to umls_filtering. Added more parameters to the umls filtering script

--- term_extraction_ner/umls_filtering/code/merge_umls_files.py
+++ term_extraction_ner/umls_filtering/code/merge_umls_files.py
@@ -1,3 +1,4 @@
+import codecs
 import os
 from argparse import ArgumentParser
 import pandas as pd
@@ -7,27 +8,35 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--groups_file', required=True)
     parser.add_argument('--vocab_path', required=True)
+    parser.add_argument('--keep_columns', nargs='+', required=True)
+    parser.add_argument('--column_name_mapping_file', required=True)
     parser.add_argument('--output_path', required=True)
     args = parser.parse_args()
 
     groups_path = args.groups_file
     vocab_path = args.vocab_path
+    keep_columns_list = args.keep_columns
+    column_name_mapping_path = args.column_name_mapping_file
     output_path = args.output_path
     output_dir = os.path.dirname(output_path)
     if not os.path.exists(output_dir) and not output_dir == '':
         os.makedirs(output_dir)
+    column_rename_map = {}
+    with codecs.open(column_name_mapping_path, 'r', encoding='utf-8') as inp_file:
+        for line in inp_file:
+            line = line.strip()
+            attrs = line.split("|")
+            rename_from = attrs[0]
+            rename_to = attrs[1]
+            column_rename_map[rename_from] = rename_to
+
     groups_df = pd.read_csv(groups_path, sep='|', header=None,
                             names=['Semantic Group Abbrev', 'Semantic Group Name', 'TUI', 'Full Semantic Type Name'])
     vocab_df = pd.read_csv(vocab_path, sep='\t', )
     merged_df = pd.DataFrame.merge(groups_df, vocab_df, on="TUI")
-    filtered_merged_df = merged_df[
-        ['CUI', 'TUI', 'Semantic Group Abbrev', 'SCUI', 'STR', 'Full Semantic Type Name',
-         'Semantic Group Name']]
+    filtered_merged_df = merged_df[keep_columns_list]
     filtered_merged_df.rename(
-        columns={'Semantic Group Abbrev': 'semantic_group', 'CUI': 'umls_cui',
-                 'Semantic Group': 'semantic_group_name', 'SCUI': 'meshrus_cui',
-                 'Full Semantic Type Name': 'semantic_type_name',
-                 'TUI': 'semantic_type', 'STR': 'term', 'Semantic Group Name' : 'semantic_group_name'}, inplace=True)
+        columns=column_rename_map, inplace=True)
     print(filtered_merged_df.head())
     filtered_merged_df.to_csv(output_path, sep='\t', index=None)
 
