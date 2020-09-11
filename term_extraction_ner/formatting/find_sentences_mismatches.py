14df5a88ff97d53739ac2d981224549e6f2eaa6b
An <an@clab>
2020-09-11 11:23:06 +0300

    Limited number of iteartion in rudrec and conll sentences number comparison

--- term_extraction_ner/formatting/find_sentences_mismatches.py
+++ term_extraction_ner/formatting/find_sentences_mismatches.py
@@ -18,13 +18,22 @@ def main():
             codecs.open(rudrec_sentences_path, 'r', encoding='utf-8') as rudrec_file:
         biobert_sentence = biobert_file.readline().strip()
         rudrec_sentence = rudrec_file.readline().strip()
+        previous_rudrec = None
+        previous_biobert = None
         i = 1
         while biobert_sentence is not None and rudrec_sentence is not None:
-            if check_sentences_matching(biobert_sentence=biobert_sentence, rudrec_sentence=rudrec_sentence):
-                biobert_sentence = biobert_file.readline().strip()
-                if not check_sentences_matching(biobert_sentence=biobert_sentence, rudrec_sentence=rudrec_sentence):
-                    raise Exception("EXCEPTION")
+            if i < 1000:
+                if check_sentences_matching(biobert_sentence=biobert_sentence, rudrec_sentence=rudrec_sentence):
+                    print('Previous biobert', previous_biobert)
+                    print('Previous rudrec', previous_rudrec)
+                    biobert_sentence = biobert_file.readline().strip()
+                    if not check_sentences_matching(biobert_sentence=biobert_sentence, rudrec_sentence=rudrec_sentence):
+                        raise Exception("EXCEPTION")
+            else:
+                break
             i += 1
+            previous_rudrec = rudrec_sentence
+            previous_biobert = biobert_sentence
             rudrec_sentence = rudrec_file.readline().strip()
             biobert_sentence = biobert_file.readline().strip()
 
