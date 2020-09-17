19182e4f4f789e16d3c69d34c1d9b7f6f914bf90
An <an@clab>
2020-09-17 20:33:18 +0300

    Modified rudrec_to_json script to take input corpus in an arbitrary format

--- term_extraction_ner/formatting/rudrec_to_conll_nlpdatasetio.py
+++ term_extraction_ner/formatting/rudrec_to_conll_nlpdatasetio.py
@@ -6,17 +6,19 @@ from NLPDatasetIO.dataset import Dataset
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--rudrec_json', default=r'data_test.json')
+    parser.add_argument('--input_path', default=r'data_test.json')
     parser.add_argument('--output_path', default=r'../data/otzovik_conll/test.tsv')
+    parser.add_argument('--input_format', default='json')
     args = parser.parse_args()
 
-    rudrec_json_path = args.rudrec_json
+    input_path = args.input_path
+    input_format = args.input_format
     output_path = args.output_path
     output_dir = os.path.dirname(output_path)
     if not os.path.exists(output_dir) and not output_dir == '':
         os.makedirs(output_dir)
 
-    dataset = Dataset(rudrec_json_path, 'json')
+    dataset = Dataset(input_path, input_format)
     dataset.save('conll', path_to_save=output_path, sep='\t')
 
 
