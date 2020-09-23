c8aebc3e776b647128bb832bd9b58cbb82a628d1
An <an@clab>
2020-09-23 18:19:16 +0300

    Added configparser to predicted_biobert_to_json script

--- term_extraction_ner/entities_statistics/code/predicted_biobert_to_json.py
+++ term_extraction_ner/entities_statistics/code/predicted_biobert_to_json.py
@@ -1,16 +1,33 @@
 import codecs
 import dataclasses
 import json
+import os
+from argparse import ArgumentParser
 
 from NLPDatasetIO.dataset import Dataset
 
 
 def main():
-    predicted_test_set = Dataset('../predicted_biobert.txt', 'conll')
-    output_filename = 'entities.json'
-    num_docs_filename = 'entities_num_docs.txt'
-    with codecs.open(output_filename, 'w+', encoding='utf-8') as output_file, \
-            codecs.open(num_docs_filename, 'w+', encoding='utf-8') as stats_file:
+    parser = ArgumentParser()
+    parser.add_argument('--predicted_path', default='../../rudrec_markup/predicted_biobert.txt')
+    parser.add_argument('--output_path', default=r'entities.json')
+    parser.add_argument('--output_num_docs', default='entities_num_docs.txt')
+    args = parser.parse_args()
+
+    predicted_path = args.predicted_path
+    output_num_docs_path = args.output_num_docs
+    output_dir = os.path.dirname(output_num_docs_path)
+    if not os.path.exists(output_dir) and not output_dir == '':
+        os.makedirs(output_dir)
+    output_path = args.output_path
+    output_dir = os.path.dirname(output_path)
+    if not os.path.exists(output_dir) and not output_dir == '':
+        os.makedirs(output_dir)
+
+    predicted_test_set = Dataset(predicted_path, 'conll')
+
+    with codecs.open(output_path, 'w+', encoding='utf-8') as output_file, \
+            codecs.open(output_num_docs_path, 'w+', encoding='utf-8') as stats_file:
         for document in predicted_test_set.documents:
             doc_dict = {"sent_id": document.doc_id,
                         "sent_text": document.text}
