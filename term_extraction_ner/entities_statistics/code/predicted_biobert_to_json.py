6a6ee4531890a38d105f9a05e6bddd4771e5d811
An <an@clab>
2020-09-23 17:56:42 +0300

    Removed sentences with no entities from predicted_biobert_to_json script; removed label from entity dict

--- term_extraction_ner/entities_statistics/code/predicted_biobert_to_json.py
+++ term_extraction_ner/entities_statistics/code/predicted_biobert_to_json.py
@@ -8,18 +8,22 @@ from NLPDatasetIO.dataset import Dataset
 def main():
     predicted_test_set = Dataset('../predicted_biobert.txt', 'conll')
     output_filename = 'entities.json'
-    with codecs.open(output_filename, 'w+', encoding='utf-8') as output_file:
+    num_docs_filename = 'entities_num_docs.txt'
+    with codecs.open(output_filename, 'w+', encoding='utf-8') as output_file, \
+            codecs.open(num_docs_filename, 'w+', encoding='utf-8') as stats_file:
         for document in predicted_test_set.documents:
-            doc_dict = {"doc_id": document.doc_id,
-                        "doc_text": document.text}
+            doc_dict = {"sent_id": document.doc_id,
+                        "sent_text": document.text}
             entities = []
             for entity in document.entities:
                 entity_dict = dataclasses.asdict(entity)
+                del entity_dict["label"]
                 entities.append(entity_dict)
             doc_dict["entities"] = entities
-            json.dump(doc_dict, output_file, ensure_ascii=False)
-            output_file.write('\n')
-
+            if len(entities) > 0:
+                json.dump(doc_dict, output_file, ensure_ascii=False)
+                output_file.write('\n')
+        stats_file.write(f"Num sentences: {len(predicted_test_set.documents)}\n")
 
 
 if __name__ == '__main__':
