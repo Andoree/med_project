fd7767dbba38291e6aa96e161359ca5a6b2b06bd
An <an@clab>
2020-10-20 10:45:26 +0300

    Updated filtering script

--- term_extraction_ner/wn_meddra_stats/code/filter_vocab.py
+++ term_extraction_ner/wn_meddra_stats/code/filter_vocab.py
@@ -7,22 +7,24 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--meddra_path',
                         default=r'../vocabs/vocabs_w_metadata/lemmatized_ru_meddra_vocab.tsv')
-    parser.add_argument('--keep_types', nargs='+', default=["PT", "LLT"])
+    parser.add_argument('--filter_column', default="term_type")
+    parser.add_argument('--keep_values', nargs='+', default=["PT", "LLT"])
     parser.add_argument('--output_path',
                         default=r'../vocabs/vocabs_w_metadata/pt_llt_lemmatized_ru_meddra_vocab.tsv')
     args = parser.parse_args()
 
     input_meddra_path = args.meddra_path
-    keep_types_list = args.keep_types
+    filter_column = args.filter_column
+    keep_values_list = args.keep_values
     output_path = args.output_path
     output_dir = os.path.dirname(output_path)
     if not os.path.exists(output_dir) and not output_dir == '':
         os.makedirs(output_dir)
 
     meddra_df = pd.read_csv(input_meddra_path, sep='\t', encoding="utf-8")
-    print(f"Meddra vocab size: {meddra_df.shape[0]}")
-    filtered_meddra = meddra_df[meddra_df.term_type.isin(keep_types_list)]
-    print(f"Meddra vocab size: {filtered_meddra.shape[0]}")
+    print(f"Vocab size before: {meddra_df.shape[0]}")
+    filtered_meddra = meddra_df[meddra_df[filter_column].isin(keep_values_list)]
+    print(f"Vocab size after: {filtered_meddra.shape[0]}")
     filtered_meddra.to_csv(output_path, sep='\t', encoding="utf-8", index=False)
 
 
--- term_extraction_ner/wn_meddra_stats/code/meddra_count_intersection_by_type.py
+++ term_extraction_ner/wn_meddra_stats/code/meddra_count_intersection_by_type.py
@@ -9,6 +9,7 @@ def main():
                         default=r'../vocabs/vocabs_w_metadata/lemmatized_ru_meddra_vocab.tsv')
     parser.add_argument('--wn_meddra_intersection',
                         default=r'../wordnet_meddra/all_terms_wordnet_lemm_meddra_intersection.tsv')
+    parser.add_argument('--term_types', nargs='+', default=["PT", "LLT"])
     parser.add_argument('--output_df_path',
                         default=r'../wordnet_meddra/meddra_stats/wn_meddra_intersection_by_senses.tsv')
     parser.add_argument('--output_final_path',
@@ -18,6 +19,7 @@ def main():
 
     meddra_path = args.meddra_path
     wn_meddra_intersection_path = args.wn_meddra_intersection
+    term_types_list = args.term_types
     output_senses_in_meddra_path = args.output_df_path
     output_dir = os.path.dirname(output_senses_in_meddra_path)
     if not os.path.exists(output_dir) and not output_dir == '':
@@ -49,18 +51,24 @@ def main():
     all_covered_cuis_set = set(joined_df.umls_cui.values)
     print(f"Unique meddra codes: {len(unique_meddra_codes)}")
     # Finding a set codes of PT instances of the wordnet-meddra intersection
-    pt_df = joined_df[joined_df["term_type"] == "PT"]
-    unique_pt_codes = set(pt_df.ru_meddra_code.values)
-    unique_cuis_of_pt_codes_set = set(pt_df.umls_cui.values)
-    print(f"Unique pt meddra codes: {len(unique_pt_codes)}")
+    pt_df = joined_df[joined_df["term_type"].isin(term_types_list)]
+    unique_pt_codes_from_intersection_set = set(pt_df.ru_meddra_code.values)
+    unique_cuis_of_pt_codes_from_intersection_set = set(pt_df.umls_cui.values)
+    print(f"Unique pt meddra codes: {len(unique_pt_codes_from_intersection_set)}")
     print(f"Unique meddra_codes: {len(unique_meddra_codes)}")
-    print(f"Unique pt / Unique meddra codes: {len(unique_pt_codes) / len(unique_meddra_codes)}")
+    print(f"Unique pt / Unique meddra codes: {len(unique_pt_codes_from_intersection_set) / len(unique_meddra_codes)}")
+    full_meddra_pt_df = meddra_df[meddra_df["term_type"].isin(term_types_list)]
+    full_meddra_pt_df = full_meddra_pt_df[full_meddra_pt_df.ru_meddra_code.isin(unique_meddra_codes)]
+    unique_pt_codes_from_meddra_set = set(full_meddra_pt_df.ru_meddra_code.values)
+    print(
+        f"All meddra Unique pt / Unique meddra codes: {len(unique_pt_codes_from_meddra_set) / len(unique_meddra_codes)}")
+
     # Excluding CUIS that has a term with PT type from wordnet-lemma intersection
-    all_covered_cuis_set = all_covered_cuis_set.difference(unique_cuis_of_pt_codes_set)
+    all_covered_cuis_set = all_covered_cuis_set.difference(unique_cuis_of_pt_codes_from_intersection_set)
     # Getting meddra terms with CUIs that do not have a PT in wordnet-meddra intersection
     meddra_pt_codes_df = meddra_df[meddra_df.umls_cui.isin(all_covered_cuis_set)]
     # Filtering out all terms that are not of type PT
-    meddra_pt_codes_df = meddra_pt_codes_df[meddra_pt_codes_df.term_type == "PT"]
+    meddra_pt_codes_df = meddra_pt_codes_df[meddra_pt_codes_df.term_type.isin(term_types_list)]
     # Getting a list of CUIs that have a PT but not in wordnet-meddra intersection
     meddra_cuis_with_pt_set = set(meddra_pt_codes_df.umls_cui.values)
 
@@ -73,9 +81,12 @@ def main():
 
     joined_df.to_csv(output_senses_in_meddra_path, sep='\t', encoding="utf-8", index=False)
     with open(output_stats_path, 'w+', encoding="utf-8") as output_file:
-        output_file.write(f"Unique pt meddra codes: {len(unique_pt_codes)}\n")
+        output_file.write(f"Unique pt meddra codes: {len(unique_pt_codes_from_intersection_set)}\n")
         output_file.write(f"Unique meddra_codes: {len(unique_meddra_codes)}\n")
-        output_file.write(f"Unique pt / Unique meddra codes: {len(unique_pt_codes) / len(unique_meddra_codes)}\n")
+        output_file.write(
+            f"Unique pt / Unique meddra codes: {len(unique_pt_codes_from_intersection_set) / len(unique_meddra_codes)}\n")
+        output_file.write(
+            f"All meddra Unique pt / Unique meddra codes: {len(unique_pt_codes_from_meddra_set) / len(unique_meddra_codes)}\n")
         output_file.write(f"No PT codes / Unique meddra codes: {len(no_pt_codes_set) / len(unique_meddra_codes)}\n")
         output_file.write(
             f"terms, that have a PT with the same CUI / Unique meddra codes: {1 - len(no_pt_codes_set) / len(unique_meddra_codes)}\n")
